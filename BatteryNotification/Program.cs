b8237584a7728df8ecedd7ddbb8e815ef5627a10
a1v0 <avc285@gmail.com>
2024-11-24 10:38:51 +0000

    Validate args

--- BatteryNotification/Program.cs
+++ BatteryNotification/Program.cs
@@ -4,26 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            // 
-            // TODO:
-            // retrieve args and throw error or call method
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No arguments given. Please provide argument and try again.");
+                return;
+            }
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Too many arguments given. Please provide one argument and try again.");
+                return;
+            }
+
+            string arg = args[0];
+            switch (arg)
+            {
+                case "check-full":
+                    // TODO:
+                    // rename argument
+                    // it doesn't sound very good
+                    break;
+                default:
+                    Console.WriteLine($"'{arg}' is not a recognised argument.");
+                    return;
+            }
         }
     }
 }
