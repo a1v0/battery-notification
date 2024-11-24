8be110ae39f56df5642564cc15e505baef241058
a1v0 <avc285@gmail.com>
2024-11-24 12:58:24 +0000

    Send notifications

--- BatteryNotification/BatteryChecker.cs
+++ BatteryNotification/BatteryChecker.cs
@@ -10,10 +10,12 @@ namespace BatteryNotification
                 Console.WriteLine("Battery Life Percent: {0}%", status.BatteryLifePercent);
                 Console.WriteLine("Battery Life Time: {0} seconds", status.BatteryLifeTime);
                 Console.WriteLine(status.ACLineStatus); // This determines whether the battery is charging: 1=yes, 0=no
+                NotificationHandler.Notify("SOME SORT OF MESSAGE");
             }
             else
             {
                 Console.WriteLine("Unable to get battery status.");
+                NotificationHandler.Notify("Unable to get battery status."); // This could become irritating if inability to access the battery status is a frequent occurrence
             }
             // 
             // TODO:
