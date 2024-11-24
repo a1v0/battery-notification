50f0674489417099fa9582a7763f2995ba0a3d36
a1v0 <avc285@gmail.com>
2024-11-24 13:16:07 +0000

    Rename argument

--- BatteryNotification/Program.cs
+++ BatteryNotification/Program.cs
@@ -40,21 +40,8 @@ namespace BatteryNotification
             string arg = args[0];
             switch (arg)
             {
-                case "check-full":
-                    // TODO:
-                    // rename argument
-                    // it doesn't sound very good
+                case "full-battery":
                     BatteryChecker.HandleFullBattery();
-                    // 
-                    // TODO:
-                    // create notification class to send resultant message
-                    // 
-                    // 
-                    // 
-                    // 
-                    // 
-                    // 
-                    // 
                     break;
                 default:
                     Console.WriteLine($"'{arg}' is not a recognised argument.");
