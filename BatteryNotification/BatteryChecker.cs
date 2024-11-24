2251ee2d26e7a964afa7fa14994ea18d7cc0479b
a1v0 <avc285@gmail.com>
2024-11-24 18:12:17 +0000

    Remove console logs

--- BatteryNotification/BatteryChecker.cs
+++ BatteryNotification/BatteryChecker.cs
@@ -11,14 +11,11 @@ namespace BatteryNotification
             {
                 byte batteryLife = status.BatteryLifePercent; // TODO: This returns 0 to 100, or 255 if unknown. Need to handle 255
 
-                // Console.WriteLine($"Battery Life Percent: {batteryLife}%");
-                
                 bool isCharging = status.ACLineStatus != 0; // 0 is not charging; 1 is charging; 255 is unknown, which I am treating the same as 1
                 // N.B.: status.BatteryFlag also contains useful charging info. ACLineStatus will return 1 even when there's no battery
 
                 if (!isCharging)
                 {
-                    // Console.WriteLine("Battery not charging.");
                     return;
                 }
 
@@ -30,7 +27,6 @@ namespace BatteryNotification
             }
             else
             {
-                // Console.WriteLine("Unable to get battery status.");
                 NotificationHandler.Notify("Unable to get battery status."); // This could become irritating if inability to access the battery status is a frequent occurrence
             }
         }
