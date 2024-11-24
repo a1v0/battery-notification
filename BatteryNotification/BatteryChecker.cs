f807bde33f5fb479ca2bf8f67f4ad8b89d83f2b7
a1v0 <avc285@gmail.com>
2024-11-24 13:15:31 +0000

    Refactor messages

--- BatteryNotification/BatteryChecker.cs
+++ BatteryNotification/BatteryChecker.cs
@@ -11,7 +11,7 @@ namespace BatteryNotification
             {
                 byte batteryLife = status.BatteryLifePercent;
 
-                Console.WriteLine("Battery Life Percent: {0}%", batteryLife);
+                Console.WriteLine($"Battery Life Percent: {batteryLife}%");
                 
                 bool isCharging = status.ACLineStatus == 1; // TODO: check if there can be any other value other than 1 and 0. Should the check actually be != 0? Does this return 0 if battery is at 100%?
 
@@ -25,35 +25,13 @@ namespace BatteryNotification
 
                 if (!thresholdIsMet) return;
 
-                NotificationHandler.Notify("Battery Charge: {0}%.\n\nPlease unplug device.", batteryLife);
+                NotificationHandler.Notify($"Battery Charge: {batteryLife}%.\n\nPlease unplug device.");
             }
             else
             {
                 Console.WriteLine("Unable to get battery status.");
                 NotificationHandler.Notify("Unable to get battery status."); // This could become irritating if inability to access the battery status is a frequent occurrence
             }
-            // 
-            // TODO:
-            // re-evaluate method name and return value:
-            // - probably doesn't need to return anything
-            // - if no return value, name shouldn't be "check"
-            // - this perhaps also has ramifications for the "check-full" arg
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
-            // 
         }
     }
 }
