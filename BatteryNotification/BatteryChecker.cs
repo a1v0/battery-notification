f8c0cdd9a5e5143cfb6680121332527bf4915b2e
a1v0 <avc285@gmail.com>
2024-11-24 11:34:47 +0000

    Create placeholder method `CheckFull`

--- BatteryNotification/BatteryChecker.cs
+++ BatteryNotification/BatteryChecker.cs
@@ -2,6 +2,31 @@ namespace BatteryNotification
 {
     public class BatteryChecker
     {
-
+        public static bool CheckFull()
+        {
+            // 
+            // TODO:
+            // improve placeholder
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            return true;
+        }
     }
 }
