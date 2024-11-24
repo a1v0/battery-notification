bb07a618a3b3ccf8cc266438d5e5bb6fa015c65c
a1v0 <avc285@gmail.com>
2024-11-24 13:37:57 +0000

    Add comments and to-dos

--- BatteryNotification/BatteryChecker.cs
+++ BatteryNotification/BatteryChecker.cs
@@ -9,11 +9,12 @@ namespace BatteryNotification
             Program.SYSTEM_POWER_STATUS status;
             if (Program.GetSystemPowerStatus(out status))
             {
-                byte batteryLife = status.BatteryLifePercent;
+                byte batteryLife = status.BatteryLifePercent; // TODO: This returns 0 to 100, or 255 if unknown. Need to handle 255
 
                 Console.WriteLine($"Battery Life Percent: {batteryLife}%");
                 
                 bool isCharging = status.ACLineStatus != 0; // 0 is not charging; 1 is charging; 255 is unknown, which I am treating the same as 1
+                // N.B.: status.BatteryFlag also contains useful charging info. ACLineStatus will return 1 even when there's no battery
 
                 if (!isCharging)
                 {
