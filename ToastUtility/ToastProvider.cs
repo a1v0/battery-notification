3703fdd20ace86c567d463985b2ef975fd043a17
avc285 <avc285@gmail.com>
2024-12-14 22:06:13 +0000

    Create `ToastMessage` property

--- ToastUtility/ToastProvider.cs
+++ ToastUtility/ToastProvider.cs
@@ -1,7 +1,14 @@
+using Microsoft.Toolkit.Uwp.Notifications;
+
 namespace ToastUtility
 {
     public class ToastProvider
     {
+        private ToastContentBuilder ToastMessage
+        {
+            get;
+        } = new ToastContentBuilder();
+
         public enum Type
         {
             FullBattery
