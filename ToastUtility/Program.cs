ae0df2fd32df83a9706f1879f88667cc92312dd4
avc285 <avc285@gmail.com>
2024-12-14 15:35:14 +0000

    Add to-dos

--- ToastUtility/Program.cs
+++ ToastUtility/Program.cs
@@ -6,10 +6,65 @@ namespace ToastUtility
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                // 
+                // TODO:
+                // trigger notification 
+                // 
+                // 
+                // 
+                // 
+                // 
+                // 
+                // 
+                // 
+                // 
+                // 
+                // 
+                // 
+                return;
+            }
+            // 
+            // 
+            // TODO:
+            // cycle through args and trigger notifications accordingly
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+
+
+
+
+
+
+
+
+
+
+
+
+
             new ToastContentBuilder()
                 .AddArgument("action", "viewConversation")
                 .AddArgument("conversationId", 9813)
-                .AddText("Andrew sent you a picture")
+                .AddText("Andrew sent you a picturesdmsd")
                 .AddText("Check this out, The Enchantments in Washington!")
                 .Show();
         }
