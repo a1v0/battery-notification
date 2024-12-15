40bb0b15e3b2c37d7390d6698482fc6d7b6f9737
avc285 <avc285@gmail.com>
2024-12-15 12:41:33 +0000

    Change how args are parsed

--- ToastUtility/Program.cs
+++ ToastUtility/Program.cs
@@ -15,26 +15,27 @@
                 return;
             }
 
-            foreach (string arg in args)
+            string rawToastType = args[0];
+            string? toastData = args.Length == 2 ? args[1] : null;
+
+            ToastProvider toastProvider = new();
+            try
             {
-                ToastProvider toastProvider = new();
-                try
-                {
-                    int toastType = int.Parse(arg);
-                    toastProvider.ShowToast((ToastProvider.Type)toastType);
-                }
-                catch
-                {
-                    // 
-                    // TODO:
-                    // This catch block occasionally catches other errors
-                    // down the line, at which point the value of "args"
-                    // isn't what it should be. Not sure what's causing
-                    // this to happen, but it's currently not an important
-                    // bug.
-                    // 
-                    toastProvider.ShowToast(ToastProvider.Type.InvalidArgsError, arg);
-                }
+                int toastType = int.Parse(rawToastType);
+                toastProvider.ShowToast((ToastProvider.Type)toastType, toastData);
+            }
+            catch
+            {
+                // 
+                // TODO:
+                // This catch block occasionally catches other errors
+                // down the line, at which point the value of "args"
+                // isn't what it should be. Not sure what's causing
+                // this to happen, but it's currently not an important
+                // bug.
+                // 
+                string errorData = toastData == null ? rawToastType : $"{rawToastType},{toastData}";
+                toastProvider.ShowToast(ToastProvider.Type.InvalidArgsError, errorData);
             }
         }
     }
