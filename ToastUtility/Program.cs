7147c5ce2e553e1ee513fdfc544727b51d4ffb72
avc285 <avc285@gmail.com>
2024-12-15 10:48:24 +0000

    Remove obsolete Toast code

--- ToastUtility/Program.cs
+++ ToastUtility/Program.cs
@@ -1,34 +1,9 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
-
-namespace ToastUtility
+﻿namespace ToastUtility
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            new ToastContentBuilder()
-                .SetToastScenario(ToastScenario.Alarm)
-                .AddText("Andrew sent you a picturesdmsd")
-                .AddText("Andrew sent you a pictures")
-                .AddButton(new ToastButtonDismiss())
-                .AddAudio(new Uri(@"C:\Windows\Media\Windows Notify System Generic.wav"))
-                // 
-                // TODO:
-                // un-hard-code this URI
-                // 
-                // 
-                // 
-                // 
-                // 
-                // 
-                // 
-                // 
-                // 
-                // 
-                // 
-                // 
-                .AddAppLogoOverride(new Uri(@"C:\Users\mensu\Desktop\battery-notification\ToastUtility\toast-icon.png"), ToastGenericAppLogoCrop.Circle)
-                .Show();
             if (args.Length < 1)
             {
                 // 
